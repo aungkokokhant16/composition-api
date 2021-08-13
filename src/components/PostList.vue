3fce3370842ee2e546a9b8197a3ff561e5529923
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2021-08-13 22:51:59 +0630

    Using Props in the Setup Function

---
+++ src/components/PostList.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>PostList</h1>
+  <div v-for="post in posts" :key="post.id">
+    <SinglePost :post="post"></SinglePost>
+  </div>
+</template>
+
+<script>
+import SinglePost from './SinglePost'
+export default {
+  components: { SinglePost },
+    props:["posts"],
+    setup(props){
+        console.log(props.posts)
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SinglePost.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>{{post.title}}</h2>
+  <p>{{cupPostBody}}</p>
+</template>
+
+<script>
+import { computed } from '@vue/reactivity'
+export default {
+    props:['post'],
+    setup(props){
+        let cupPostBody = computed(()=>{
+
+            return props.post.body.substring(0,100)+"......";
+        })
+
+        return {cupPostBody}
+    }
+}
+</script>
+
+<style>
+
+</style>
